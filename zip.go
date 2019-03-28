7126d9588bb07f8d09d2b857d9f86b018bfccdba
MiguelKim <ksooh88@naver.com>
2019-03-28 17:20:06 +0900

    init

---
+++ zip.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+
+	var buff bytes.Buffer
+
+	zipW := zip.NewWriter(&buff)
+	f, err := zipW.Create("newfile.txt")
+	if err != nil {
+		panic(err)
+	}
+	_, err = f.Write([]byte("새 압축파일"))
+	if err != nil {
+		panic(err)
+	}
+	err = zipW.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile("data.zip", buff.Bytes(), os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	zipR, err := zip.OpenReader("data.zip")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range zipR.File {
+		fmt.Println("File " + file.Name + " contains:")
+		r, err := file.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = io.Copy(os.Stdout, r)
+		if err != nil {
+			panic(err)
+		}
+		err = r.Close()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println()
+	}
+
+}
